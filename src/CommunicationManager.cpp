61f2b52a6ce8444c437afb36ac5deb1715714224
vivek-shankar <robovivek@gmail.com>
2017-01-29 19:01:51 -0500

    change in loop rate

--- src/CommunicationManager.cpp
+++ src/CommunicationManager.cpp
@@ -44,7 +44,7 @@ namespace Xbee
 //*****************************************************************************
 CommunicationManager::CommunicationManager():
 	START_DLIMITER(static_cast<unsigned char>(0x7E)),
-	LOOP_RATE(10) /* 10 fps */
+	LOOP_RATE(30) /* 10 fps */
 {
 }
 
