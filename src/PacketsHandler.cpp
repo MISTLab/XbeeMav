0d5de6d487bb63e787b57d87b4dc0465bc928e4d
vivek-shankar <robovivek@gmail.com>
2017-02-18 18:33:13 -0500

    hardoded location for database fix

--- src/PacketsHandler.cpp
+++ src/PacketsHandler.cpp
@@ -468,7 +468,7 @@ void PacketsHandler::Send_End_Of_Packet_Ping(const uint8_t packet_ID, const uint
 //*****************************************************************************
 bool PacketsHandler::Load_Database_Addresses()
 {
-	const std::string FILE_PATH = "~/ROS_WS/src/xbeemav/Resources/database.xml";
+	const std::string FILE_PATH = "/home/ubuntu/ROS_WS/src/xbeemav/Resources/database.xml";
 	
 	if (!boost::filesystem::exists(FILE_PATH))
 	{	
