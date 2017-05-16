be3f85a6ff424a833a0d2fa7eb4263d446cc7852
vivek-shankar <robovivek@gmail.com>
2017-05-16 17:37:59 -0400

    debug

--- src/PacketsHandler.cpp
+++ src/PacketsHandler.cpp
@@ -270,7 +270,7 @@ void PacketsHandler::Process_Ping_Or_Acknowledgement(std::shared_ptr<std::string
 				assembly_map_it_->second.time_since_creation_ = 0;
 				//if(!cur_frame.empty()) cur_frames.clear();
 			}
-			else if(!assembly_map_it_->second.packet_buffer_.empty())
+			else if(!assembly_map_it_->second.received_fragments_IDs_.empty())
 			{
 				std::set<uint8_t>::iterator it = assembly_map_it_->second.received_fragments_IDs_.begin();
 				uint8_t j = 0;
